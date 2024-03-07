7624b57d292287cec7ebd184e0a37b73440f8578
Kevin Cawley <162336743+kevinccawley@users.noreply.github.com>
2024-03-07 14:50:49 -0500

    Update top_50_loyalty_points.sql

--- top_50_loyalty_points.sql
+++ top_50_loyalty_points.sql
@@ -1,17 +1,43 @@
-SELECT
-  client_id,
-  first_name,
-  last_name,
-  email,
-  SUM(IFNULL(`phorest-techtest.source_data.services`.loyalty_points,0)) AS services_loyalty,
-  SUM(IFNULL(`phorest-techtest.source_data.purchases`.loyalty_points,0)) AS purchases_loyalty,
-  SUM(IFNULL(`phorest-techtest.source_data.services`.loyalty_points,0)) + SUM(IFNULL(`phorest-techtest.source_data.purchases`.loyalty_points,0)) AS total_loyalty
-FROM `phorest-techtest.source_data.clients`
-LEFT JOIN `phorest-techtest.source_data.appointments` ON `phorest-techtest.source_data.appointments`.client_id = `phorest-techtest.source_data.clients`.id
-LEFT JOIN `phorest-techtest.source_data.services` ON `phorest-techtest.source_data.appointments`.id = `phorest-techtest.source_data.services`.appointment_id
-LEFT JOIN `phorest-techtest.source_data.purchases` ON `phorest-techtest.source_data.appointments`.id = `phorest-techtest.source_data.purchases`.appointment_id
-WHERE banned = FALSE
-AND start_time >= TIMESTAMP('2018-01-01 00:00:00')
-GROUP BY client_id, last_name, first_name, email
-ORDER BY total_loyalty
-DESC LIMIT 70
+SELECT 
+    c.id, 
+    c.email,
+    c.first_name,
+    c.last_name,
+    COALESCE(s.services_loyalty_points, 0) AS services_loyalty_points,
+    COALESCE(p.purchases_loyalty_points, 0) AS purchases_loyalty_points,
+    COALESCE(s.services_loyalty_points, 0) + COALESCE(p.purchases_loyalty_points, 0) AS total_loyalty_points
+FROM 
+    `phorest-techtest.source_data.clients` c
+LEFT JOIN (
+    SELECT 
+        a.client_id,
+        SUM(s.loyalty_points) AS services_loyalty_points
+    FROM 
+        `phorest-techtest.source_data.appointments` a
+    JOIN 
+        `phorest-techtest.source_data.services` s 
+        ON a.id = s.appointment_id
+    WHERE 
+        a.start_time >= TIMESTAMP('2018-01-01 00:00:00')
+    GROUP BY 
+        a.client_id
+) s ON c.id = s.client_id
+LEFT JOIN (
+    SELECT 
+        a.client_id,
+        SUM(p.loyalty_points) AS purchases_loyalty_points
+    FROM 
+        `phorest-techtest.source_data.appointments` a
+    LEFT JOIN 
+        `phorest-techtest.source_data.purchases` p 
+        ON a.id = p.appointment_id
+    WHERE 
+        a.start_time >= TIMESTAMP('2018-01-01 00:00:00')
+    GROUP BY 
+        a.client_id
+) p ON c.id = p.client_id
+WHERE 
+    c.banned = FALSE
+ORDER BY 
+    total_loyalty_points DESC
+LIMIT 50;
